4f3a7e813cd68d5f720dc23e105698d860cd6988
marko-kraemer <markokraemer.mail@gmail.com>
2025-10-04 16:41:17 +0200

    Add enterprise page to public routes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -14,6 +14,7 @@ const PUBLIC_ROUTES = [
   '/api/auth',
   '/share', // Shared content should be public
   '/templates', // Template pages should be public
+  '/enterprise', // Enterprise page should be public
 ];
 
 // Routes that require authentication but are related to billing/trials
